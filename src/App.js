ed824dbb6775ee458bcc3409acdfcdf7dd367b00
hanamhn <hanamhndev@gmail.com>
2021-02-18 22:52:01 +0700

    react-hook

--- src/App.js
+++ src/App.js
@@ -1,51 +1,11 @@
-import React, { useState, useEffect, useCallback } from "react";
-import ChildComponent from "./components/ChildComponent";
-
-const fetchData = (type) => {
-  return fetch(`https://jsonplaceholder.typicode.com/${type}`)
-    .then((response) => response.json())
-    .then((json) => console.log(json));
-};
+import React from "react";
 
 const App = () => {
-  const [count, setCount] = useState(0);
-
-  useEffect(() => {
-    fetchData("todos");
-  }, []);
   return (
     <div>
-      <h1>Count: {count}</h1>
-      <button onClick={() => setCount(count + 1)}>Increment</button>
-      <ChildComponent title="This is the title" fetchData={fetchData} />
+      <h2>App</h2>
     </div>
   );
 };
 
 export default App;
-
-// import React, { Component } from "react";
-// import Child from "./Child";
-
-// export default class App extends Component {
-//   state = {
-//     time: new Date(),
-//   };
-
-//   componentDidMount() {
-//     setInterval(() => {
-//       this.setState({
-//         time: new Date(),
-//       });
-//     }, 1000);
-//   }
-
-//   render() {
-//     return (
-//       <div>
-//         <Child seconds={1} />
-//         {this.state.time.toString()}
-//       </div>
-//     );
-//   }
-// }
--- src/Child.js
+++
@@ -1,8 +0,0 @@
-import { memo } from "react";
-
-const Child = ({ seconds }) => {
-  console.log("I am rendering");
-  return <p>I am updating every {seconds} seconds.</p>;
-};
-
-export default memo(Child);
